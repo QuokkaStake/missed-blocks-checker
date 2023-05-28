ba56caad51e248be1e44a6cb48ae41ec6802fdf1
Techno Freak <freak12techno@gmail.com>
2023-05-29 02:15:25 +0300

    chore: fixed linting

--- pkg/config/chain_test.go
+++ pkg/config/chain_test.go
@@ -1,16 +1,21 @@
 package config
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetNameWithoutPrettyName(t *testing.T) {
+	t.Parallel()
+
 	config := &ChainConfig{Name: "test"}
 	assert.Equal(t, config.GetName(), "test", "Names do not match!")
 }
 
 func TestGetNameWithPrettyName(t *testing.T) {
+	t.Parallel()
+
 	config := &ChainConfig{Name: "test", PrettyName: "Test"}
 	assert.Equal(t, config.GetName(), "Test", "Names do not match!")
 }
