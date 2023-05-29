df377f1a30098eef40be2ec78a7fcd293d0d8edf
Techno Freak <freak12techno@gmail.com>
2023-05-29 19:06:38 +0300

    feat: test snapshot manager

---
+++ pkg/snapshot/manager_test.go
@@ -0,0 +1,50 @@
+package snapshot
+
+import (
+	configPkg "main/pkg/config"
+	"main/pkg/logger"
+	"main/pkg/metrics"
+	"main/pkg/types"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/guregu/null.v4"
+)
+
+func TestManagerHasNewerSnapshot(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetDefaultLogger()
+	config := &configPkg.ChainConfig{}
+	manager := NewManager(*log, config, nil)
+	assert.False(t, manager.HasNewerSnapshot(), "Should not have older snapshot!")
+}
+
+func TestManagerCommitNewSnapshot(t *testing.T) {
+	t.Parallel()
+
+	log := logger.GetDefaultLogger()
+	config := &configPkg.ChainConfig{StoreBlocks: 10}
+	config.RecalculateMissedBlocksGroups()
+
+	metricsManager := metrics.NewManager(*log, configPkg.MetricsConfig{Enabled: null.BoolFrom(true)})
+	manager := NewManager(*log, config, metricsManager)
+
+	manager.CommitNewSnapshot(10, Snapshot{
+		Entries: map[string]Entry{
+			"validator": Entry{Validator: &types.Validator{}},
+		},
+	})
+	manager.CommitNewSnapshot(20, Snapshot{
+		Entries: map[string]Entry{
+			"validator": Entry{Validator: &types.Validator{}},
+		},
+	})
+
+	assert.True(t, manager.HasNewerSnapshot(), "Should not have older snapshot!")
+	assert.Equal(t, manager.GetOlderHeight(), int64(10), "Height mismatch!")
+
+	report, err := manager.GetReport()
+	assert.Nil(t, err, "Error should not be presented!")
+	assert.True(t, report.Empty(), "Report should be empty!")
+}
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,10 +1,11 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func StringOfRandomLength(n int) string {
