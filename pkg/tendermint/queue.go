d5446b879971ee72fbb73e6d95a12b4528a15589
Techno Freak <freak12techno@gmail.com>
2023-07-26 20:45:47 +0300

    feat: moved queue to another file

--- pkg/tendermint/queue.go
+++ pkg/tendermint/queue.go
@@ -1,1 +1,37 @@
 package tendermint
+
+import (
+	"main/pkg/types"
+	"sync"
+)
+
+type Queue struct {
+	Data  []types.WebsocketEmittable
+	Size  int
+	Mutes sync.Mutex
+}
+
+func NewQueue(size int) Queue {
+	return Queue{Data: make([]types.WebsocketEmittable, 0), Size: size}
+}
+
+func (q *Queue) Add(emittable types.WebsocketEmittable) {
+	q.Mutes.Lock()
+
+	if len(q.Data) >= q.Size {
+		_, q.Data = q.Data[0], q.Data[1:]
+	}
+
+	q.Data = append(q.Data, emittable)
+	q.Mutes.Unlock()
+}
+
+func (q *Queue) Has(emittable types.WebsocketEmittable) bool {
+	for _, elem := range q.Data {
+		if elem.Hash() == emittable.Hash() {
+			return true
+		}
+	}
+
+	return false
+}
--- pkg/tendermint/websocket_manager.go
+++ pkg/tendermint/websocket_manager.go
@@ -10,37 +10,6 @@ import (
 	"github.com/rs/zerolog"
 )
 
-type Queue struct {
-	Data  []types.WebsocketEmittable
-	Size  int
-	Mutes sync.Mutex
-}
-
-func NewQueue(size int) Queue {
-	return Queue{Data: make([]types.WebsocketEmittable, 0), Size: size}
-}
-
-func (q *Queue) Add(emittable types.WebsocketEmittable) {
-	q.Mutes.Lock()
-
-	if len(q.Data) >= q.Size {
-		_, q.Data = q.Data[0], q.Data[1:]
-	}
-
-	q.Data = append(q.Data, emittable)
-	q.Mutes.Unlock()
-}
-
-func (q *Queue) Has(emittable types.WebsocketEmittable) bool {
-	for _, elem := range q.Data {
-		if elem.Hash() == emittable.Hash() {
-			return true
-		}
-	}
-
-	return false
-}
-
 type WebsocketManager struct {
 	logger         zerolog.Logger
 	nodes          []*WebsocketClient
@@ -77,23 +46,24 @@ func (m *WebsocketManager) Listen() {
 	}
 
 	for _, node := range m.nodes {
-		go func(node *WebsocketClient) {
-			for msg := range node.Channel {
-				m.mutex.Lock()
+		go m.ProcessNode(node)
+	}
+}
 
-				if m.queue.Has(msg) {
-					m.logger.Trace().
-						Str("hash", msg.Hash()).
-						Msg("Message already received, not sending again")
-					m.mutex.Unlock()
-					continue
-				}
+func (m *WebsocketManager) ProcessNode(node *WebsocketClient) {
+	for msg := range node.Channel {
+		m.mutex.Lock()
 
-				m.Channel <- msg
-				m.queue.Add(msg)
+		if m.queue.Has(msg) {
+			m.logger.Trace().
+				Str("hash", msg.Hash()).
+				Msg("Message already received, not sending again")
+			m.mutex.Unlock()
+			continue
+		}
 
-				m.mutex.Unlock()
-			}
-		}(node)
+		m.Channel <- msg
+		m.queue.Add(msg)
+		m.mutex.Unlock()
 	}
 }
