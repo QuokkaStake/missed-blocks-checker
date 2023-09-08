662841ef0a6ddf9b7128eb24cf928da939a15930
Techno Freak <freak12techno@gmail.com>
2023-09-09 02:43:43 +0300

    chore: added some tests

--- pkg/types/block_test.go
+++ pkg/types/block_test.go
@@ -12,3 +12,14 @@ func TestBlockHash(t *testing.T) {
 	block := Block{Height: 123}
 	assert.Equal(t, block.Hash(), "block_123", "Wrong block hash!")
 }
+
+func TestBlockSetValidators(t *testing.T) {
+	t.Parallel()
+
+	block := Block{Height: 123}
+	assert.Nil(t, block.Validators, "Validators should be nil!")
+	block.SetValidators(map[string]bool{"1": true})
+	assert.NotNil(t, block.Validators, "Validators should not be nil!")
+	assert.Len(t, block.Validators, 1, "Validators length should be 1!")
+	assert.Equal(t, block.Validators["1"], true, "Validators mismatch!")
+}
