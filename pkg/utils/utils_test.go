0607fe69a24edd5c4cd838e3ecff2a67824df081
Techno Freak <freak12techno@gmail.com>
2023-09-16 11:48:27 +0300

    chore: disabled old linters

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -216,3 +216,10 @@ func TestMaxInt64(t *testing.T) {
 	assert.Equal(t, MaxInt64(1, 2), int64(2), "Value mismatch!")
 	assert.Equal(t, MaxInt64(2, 1), int64(2), "Value mismatch!")
 }
+
+func TestMinInt64(t *testing.T) {
+	t.Parallel()
+
+	assert.Equal(t, MinInt64(1, 2), int64(1), "Value mismatch!")
+	assert.Equal(t, MinInt64(2, 1), int64(1), "Value mismatch!")
+}
