7d1415e6d3e6c387299cddb728b8528b0eeb66ed
Techno Freak <freak12techno@gmail.com>
2023-09-30 16:48:47 +0300

    chore: fixed linting

--- pkg/types/responses/validators_test.go
+++ pkg/types/responses/validators_test.go
@@ -2,8 +2,9 @@ package responses
 
 import (
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidatorsResponseUnmarshalJson(t *testing.T) {
