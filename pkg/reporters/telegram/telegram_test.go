e1f54afd6b1848d8bb64916a10bb8a237ca1023b
Sergey <freak12techno@gmail.com>
2024-12-30 18:47:37 +0300

    chore: add BotReply error test

--- pkg/reporters/telegram/telegram_test.go
+++ pkg/reporters/telegram/telegram_test.go
@@ -9,7 +9,10 @@ import (
 	"main/pkg/metrics"
 	statePkg "main/pkg/state"
 	"main/pkg/types"
+	"strings"
 	"testing"
+
+	tele "gopkg.in/telebot.v3"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
@@ -81,4 +84,43 @@ func TestReporterInitOk(t *testing.T) {
 
 	go reporter.Start()
 	reporter.Stop()
+}
+
+//nolint:paralleltest // disabled
+func TestReporterBotSendFail(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		httpmock.NewErrorResponder(errors.New("custom error")))
+
+	config := &configPkg.ChainConfig{Name: "chain", TelegramConfig: configPkg.TelegramConfig{
+		Token:  "xxx:yyy",
+		Chat:   1,
+		Admins: []int64{1},
+	}}
+	logger := loggerPkg.GetNopLogger()
+	metricsManager := metrics.NewManager(*logger, configPkg.MetricsConfig{})
+	stateManager := statePkg.NewManager(*logger, config, metricsManager, nil, nil)
+	reporter := NewReporter(config, "1.2.3", *logger, stateManager, metricsManager, nil)
+	reporter.Init()
+
+	ctx := reporter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/help",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err := reporter.BotReply(ctx, strings.Repeat("a", 5000))
+	require.Error(t, err)
 }
